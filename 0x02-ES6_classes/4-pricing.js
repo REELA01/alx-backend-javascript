8340a31f174e722e6aff60ffdbf6d4443ef0b233
Reela <sudanrela123@gmail.com>
2024-06-27 05:23:10 +0000

    tst

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(na) {
+    this._amount = na;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(na) {
+    this._currency = na;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
